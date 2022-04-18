c4b3312e2484fe8fb03977bebd4565b04c0065aa
TDK <793065367@qq.com>
2022-04-18 22:01:02 +0800

    将dida_genIDJson配置项取消默认值

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config(BaseSettings):
     # Your Config Here
     dida_phone: str
     dida_password: str
-    dida_genIDJson: bool = True
+    dida_genIDJson: bool
     class Config:
         extra = "ignore"
