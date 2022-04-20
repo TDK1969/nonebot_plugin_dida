f7754b992052d95e9de77b1007b3c77692160fd0
TDK <793065367@qq.com>
2022-04-20 16:23:01 +0800

    修复

--- __init__.py
+++ __init__.py
@@ -534,7 +534,7 @@ dida = DidaList()
 scheduler = require("nonebot_plugin_apscheduler").scheduler
 scheduler.add_job(dida.updateCookie, "cron", day_of_week=6, hour=0, minute=0, second=0, misfire_grace_time=75)
 if config.dida_genid:
-    #修复bug
+    
     logger.debug(f'[滴答清单]将生成ID文件')
     
     scheduler.add_job(dida.genProjectIDJson, "interval", minutes=1, misfire_grace_time=45)
